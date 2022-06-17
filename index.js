33e45ce8528fd198354889e7e4d31b1291272c59
Cengizhan Atakul <cengizatakul@hotmail.de>
2022-06-17 15:45:20 +0200

    fetch151Pokemon

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-// function fetchKantoPokemon(){
-//     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-//     .then(response => response.json())
-//     .then(allpokemon => console.log(allpokemon));
-//   }
-//   console.log(fetchKantoPokemon);
-
-  function fetchKantoPokemon(){
+function fetchKantoPokemon(){
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
      .then(response => response.json())
      .then(function(allpokemon){
+        console.log(allpokemon);
      allpokemon.results.forEach(function(pokemon){
        fetchPokemonData(pokemon); 
-   
+     })
     })
-})
-   }  
-   console.log(fetchKantoPokemon);
+   }
+   fetchKantoPokemon()
+
+   function fetchPokemonData(pokemon){
+    let url = pokemon.url // <--- this is saving the pokemon url to a variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+      fetch(url)
+      .then(response => response.json())
+      .then(function(pokeData){
+      console.log(pokeData);
+      })
+    }
+    
+   
+
