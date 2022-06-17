2ede120c3bc1504f735b7409cb4da07ed32c7b36
Cengizhan Atakul <cengizatakul@hotmail.de>
2022-06-17 16:06:42 +0200

    hmtl

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ function fetchKantoPokemon(){
       console.log(pokeData);
       })
     }
+    function renderPokemon(pokeData){
+      let allPokemonContainer = document.getElementById('poke-container');
+      let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
+      let pokeName = document.createElement('h4')
+      pokeName.innerText = pokeData.name
+      let pokeNumber = document.createElement('p')
+      pokeNumber.innerText = `#${pokeData.id}`
+      let pokeTypes = document.createElement('ul') 
+      //ul list will hold the pokemon types
+      createTypes(pokeData.types, pokeTypes) 
+      // helper function to go through the types array and create li tags for each one
+      pokeContainer.append(pokeName, pokeNumber, pokeTypes);   
+      //appending all details to the pokeContainer div
+      allPokemonContainer.appendChild(pokeContainer);       
+      //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
+      }
     
    
 
